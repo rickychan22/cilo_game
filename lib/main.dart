5d391408942546eec47d6535f413676c5d843613
Ricky Chan <chan.ricky.22@hotmail.com>
2020-02-22 16:22:20 -0500

    3 dice roll randomizer

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,17 +11,22 @@ void main() {
           backgroundColor: Colors.blue,
           title: Text('Dice App'),
         ),
-        body: DicePage(),
+        body: DicePageNew(),
       ),
     ),
   );
 }
 
 // in order for hot reload to work you need to be in a stateful or stateless widget
+// stateless widgets are immutable
+class DicePage extends StatelessWidget {
 
-class DicePage1 extends StatelessWidget {
+  //not stateless widget hot reload only apploed within the build
   @override
   Widget build(BuildContext context) {
+
+    int leftDiceNumber = 2;
+
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
@@ -28,7 +35,7 @@ class DicePage1 extends StatelessWidget {
             Expanded(
               child: Padding(
                 padding: const EdgeInsets.all(16.0),
-                child: Image.asset('assets/images/dice1.png'),
+                child: Image.asset('assets/images/dice$leftDiceNumber.png'),
               ),
             ),
             Expanded(
@@ -61,58 +68,68 @@ class DicePage1 extends StatelessWidget {
 }
 
 
-class DicePage extends StatefulWidget {
+//stful shortcut
+class DicePageNew extends StatefulWidget {
   @override
   _State createState() => _State();
 }
 
-class _State extends State<DicePage> {
+class _State extends State<DicePageNew> {
 
-  int left = 1;
-  int middle = 3;
-  int right = 4;
+  int leftDiceNumber = 1;
+  int middleDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void randomize_dice () {
+    leftDiceNumber = Random().nextInt(6) + 1;
+    middleDiceNumber = Random().nextInt(6) + 1;
+    rightDiceNumber = Random().nextInt(6) + 1;
+
+    print('left = ' + leftDiceNumber.toString() + ' middle = ' + middleDiceNumber.toString() + ' right = ' + rightDiceNumber.toString());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
+        Expanded(
+          child: Image.asset('assets/images/IMG_6536.jpeg'),
+        ),
         Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Expanded(
               child: Padding(
                 padding: const EdgeInsets.all(16.0),
-                child: Image.asset('assets/images/dice$left.png'),
+                child: Image.asset('assets/images/dice$leftDiceNumber.png'),
               ),
             ),
             Expanded(
               child: Padding(
                 padding: const EdgeInsets.all(16.0),
-                child: Image.asset('assets/images/dice$middle.png'),
+                child: Image.asset('assets/images/dice$middleDiceNumber.png'),
               ),
             ),
             Expanded(
               child: Padding(
                 padding: const EdgeInsets.all(16.0),
-                child: Image.asset('assets/images/dice$right.png'),
+                child: Image.asset('assets/images/dice$rightDiceNumber.png'),
               ),
             ),
           ],
         ),
-        SizedBox(
-          height: 30.0,
-        ),
         RaisedButton(
-          onPressed: (){
+          //set state calls the build method again a tells it to update from any previous values
+          //set state  marks all the locations where the variable is being used, thus when we call build it will redraw everything that was marked as being used/changed
+          color: Colors.white,
+          onPressed: () {
             setState(() {
-              left = 2;
-              middle = 6;
-              right = 1;
-
+              print('rolling dice');
+              randomize_dice();
             });
-            //inside here is the logic for what till happen when the button is pressed
-            print('Rolling $left $middle $right');
           },
-          child: Text('Roll The Dice'),
+          child: Text('Roll Dice'),
         ),
       ],
     );
